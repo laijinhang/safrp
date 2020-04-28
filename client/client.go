f372f7b510b0c67dea07e05407ee066c14c18c74
laijinhang <761774955@qq.com>
2020-04-28 11:23:29 +0800

    拆分成8个模块

--- client/client.go
+++ client/client.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+    "fmt"
     "net"
     "sync"
     "time"
 )
 
-var tcpToClientStream = make(chan TCPData, 1000)
-var tcpFromClientStream = make(chan TCPData, 1000)
+var tcpToServerStream = make(chan TCPData, 1000)
+var tcpFromServerStream = make(chan TCPData, 1000)
 
-var addr = "192.168.1.2:8888"
+var addr = "192.168.1.2:8001"
 
 type TCPData struct {
     ConnId uint64
@@ -21,11 +22,12 @@ var BufPool = sync.Pool{}
 
 
 func main() {
-    proxyClient()
+    go proxyClient()
+    Client()
 }
 
 func proxyClient() {
-    conn, err := net.Dial("tcp", ":10001")
+    conn, err := net.Dial("tcp", ":8001")
     if err != nil {
         panic(err)
     }
@@ -49,26 +51,76 @@ func Read(c net.Conn) {
             return
         }
         n, err := c.Read(buf)
-        go func() {
-            IntranetTransmitSend(buf)
-            IntranetTransmitRead()
-
-        }()
-
+        fmt.Println(n, err)
+        if n == 0 {
+            continue
+        }
+        fmt.Println(string(buf[:n]))
+        tcpFromServerStream <- TCPData{
+            ConnId: 0,
+            Data:   buf[:n],
+        }
     }
 }
 
 // 往 内网穿透服务器 发数据
 func Send(c net.Conn) {
+    for {
+        select {
+        case data := <- tcpFromServerStream:
+            _, err := c.Write(data.Data)
+            if err != nil {
+            }
+        }
+    }
+}
 
+
+func Client() {
+    for {
+        select {
+        case data := <- tcpFromServerStream:
+            c, err := net.Dial("tcp", "127.0.0.1:81")
+            if err != nil {
+                return
+            }
+            go IntranetTransmitSend(c, data.Data)
+            IntranetTransmitRead(c)
+        }
+    }
 }
 
 // 从 内网服务器 读数据
-func IntranetTransmitRead() ([]byte, error) {
-    return nil, nil
+func IntranetTransmitRead(c net.Conn) {
+    buf := make([]byte, 1024)
+    for {
+        err := c.SetReadDeadline(time.Now().Add(2 * time.Second))
+        if err != nil {
+            return
+        }
+        n, err := c.Read(buf)
+        if err != nil {
+            continue
+        }
+        fmt.Println(string(buf[:n]))
+        tcpToServerStream <- TCPData{
+            ConnId: 0,
+            Data:   buf[:n],
+        }
+        return
+    }
+    return
 }
 
 // 往 内网服务器 发数据
-func IntranetTransmitSend(buf []byte) ([]byte, error) {
-    return nil, nil
+func IntranetTransmitSend(c net.Conn, data []byte) {
+    err := c.SetWriteDeadline(time.Now().Add(2 * time.Second))
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    _, err = c.Write(data)
+    if err != nil {
+        fmt.Println(err)
+    }
 }
--- server/server.go
+++ server/server.go
@@ -12,6 +12,7 @@ var tcpFromClientStream = make(chan TCPData, 1000)
 
 var ConnPool = sync.Pool{}
 var BufPool = sync.Pool{}
+var DeadTime = make([]chan interface{}, 1001)
 
 func init() {
     for i := 1;i <= 1000;i++ {
@@ -20,23 +21,52 @@ func init() {
 }
 
 type TCPData struct {
-    ConnId uint64
+    ConnId int
     Data []byte
 }
 
 func main() {
-    //proxyServer()
-    go server()
+    go proxyServer()    // 处理外网请求，短连接服务
+    go server()         // 与穿透客户端进行交互，长连接服务
     select {}
 }
 
-// 处理 穿透内外 网
+// 处理 外网 请求
 func proxyServer() {
-
+    listen, err := net.Listen("tcp", ":10000")
+    if err != nil {
+        panic(err)
+    }
+    for {
+        client, err := listen.Accept()
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        num := -1
+        for num == -1 {
+            tNum := ConnPool.Get()
+            if tNum != nil {
+                num = tNum.(int)
+            } else {
+                time.Sleep(50 * time.Millisecond)
+            }
+        }
+        fmt.Println(client, num)
+        go func(c net.Conn, n int) {
+            defer func() {
+                ConnPool.Put(n)
+                c.Close()
+            }()
+            //go ExtranetRead(c, n)
+            ExtranetRead(c, n)
+            ExtranetSend(c, n)
+        }(client, num)
+    }
 }
 
 // 从 外网 接收数据
-func ExtranetRead(c net.Conn) {
+func ExtranetRead(c net.Conn, number int) {
     tBuf := BufPool.Get()
     buf := []byte{}
     if tBuf == nil {
@@ -45,37 +75,50 @@ func ExtranetRead(c net.Conn) {
         buf = tBuf.([]byte)
     }
 
-    err := c.SetReadDeadline(time.Now().Add(3 * time.Second))
-    if err != nil {
-        return
-    }
-    n, err := c.Read(buf)
-    if err != nil {
-        fmt.Println("error:", err)
-        return
+    for {
+        err := c.SetReadDeadline(time.Now().Add(3 * time.Second))
+        if err != nil {
+            return
+        }
+        n, err := c.Read(buf)
+        if err != nil {
+            break
+        }
+        fmt.Println("从收到外网请求")
+        tcpToClientStream <- TCPData{
+            ConnId: number,
+            Data:   buf[:n],
+        }
     }
 }
 
 // 往 外网 响应数据
-func ExtranetSend() {
-    for {
-        c.SetReadDeadline(time.Now().Add(3 * time.Second))
-        n, err := c.Read(buf1)
-        if err != nil {
-            return
-        }
-        c.SetWriteDeadline(time.Now().Add(3 * time.Second))
-        n, err = client.Write(buf1[:n])
+func ExtranetSend(c net.Conn, number int) {
+    tBuf := BufPool.Get()
+    buf := []byte{}
+    if tBuf == nil {
+        buf = make([]byte, 1024 * 10)
+    } else {
+        buf = tBuf.([]byte)
+    }
 
-        if err != nil {
-            return
+    for {
+        select {
+        case data := <- tcpFromClientStream:
+            err := c.SetWriteDeadline(time.Now().Add(3 * time.Second))
+            if err != nil {
+                continue
+            }
+            n, err := c.Write(data.Data)
+            fmt.Println("向外网发送数据")
+            fmt.Println(string(buf[:n]))
         }
     }
 }
 
-// 处理外网服务
+// 处理穿透内网服务
 func server() {
-    listen, err := net.Listen("tcp", ":81")
+    listen, err := net.Listen("tcp", ":8001")
     if err != nil {
         panic(err)
     }
@@ -84,70 +127,56 @@ func server() {
         if err != nil {
             continue
         }
-        go func(client net.Conn) {
-            c, err := net.Dial("tcp", "192.168.1.3:8888")
-
-            if err != nil {
-                fmt.Println(err)
-                return
-            }
-            defer c.Close()
-            go func() {
-            }()
-            defer client.Close()
+        go func(c net.Conn) {
+            //go Send(c)
+            Send(c)
+            Read(c)
         }(client)
     }
 }
 
 // 往 内网穿透服务器 发数据
 func Send(c net.Conn) {
+    for {
+        select {
+        case data := <- tcpToClientStream:
+            fmt.Println("向内网发送数据")
+            // 转json
+            //fmt.Println(string(data.Data))
+            //buf, err := json.Marshal(data)
+            //
+            //if err != nil {
+            //    fmt.Println(err)
+            //    continue
+            //}
+            err := c.SetWriteDeadline(time.Now().Add(2 * time.Second))
+            n, err := c.Write(data.Data)
 
+            fmt.Println(n, err)
+        }
+    }
 }
 
 // 从 内网穿透服务器 读数据
 func Read(c net.Conn) {
-    c.SetWriteDeadline(time.Now().Add(3 * time.Second))
-    n, err = c.Write(buf[:n])
-    if err != nil {
-        return
+    tBuf := BufPool.Get()
+    buf := []byte{}
+    if tBuf == nil {
+        buf = make([]byte, 1024 * 10)
+    } else {
+        buf = tBuf.([]byte)
     }
-    n, err = c.Read(buf)
-}
 
-func a()  {
     for {
-        listen, err := net.Listen("tcp", ":10001")
+        err := c.SetReadDeadline(time.Now().Add(3 * time.Second))
         if err != nil {
-            panic(err)
+            continue
         }
-        for {
-            client, err := listen.Accept()
-            if err != nil {
-                continue
-            }
-            // 读取
-            go func(c net.Conn) {
-                tBuf := BufPool.Get()
-                buf := []byte{}
-                if tBuf == nil {
-                    buf = make([]byte, 1024 * 10)
-                } else {
-                    buf = tBuf.([]byte)
-                }
-                defer BufPool.Put(buf)
-
-                for {
-                    err := c.SetReadDeadline(time.Now().Add(3 * time.Second))
-                    if err != nil {
-                        break
-                    }
-                    //n, err := c.Read(buf)
-
-                }
-            }(client)
-            // 发送
-
+        n, err := c.Read(buf)
+        fmt.Println("送内网读取数据")
+        tcpFromClientStream <- TCPData{
+            ConnId: 0,
+            Data:   buf[:n],
         }
     }
 }
-
